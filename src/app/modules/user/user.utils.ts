4c2032583a5fc626aac76d1c1701dfcd8bfa7c3c
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-12-08 18:57:27 +0600

    Fix problem related of Student id

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,11 +16,26 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
-};
+  return lastStudent?.id ? lastStudent?.id : undefined;
+}; 
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId =  await findLastStudentId();
+  // lastStudentId = 2023010001
+  const lastStudentSemesterYear = lastStudentId?.substring(0,4); // 2023 
+  const lastStudentSemesterCode = lastStudentId?.substring(4,6);  // 01
+  const currentStudentSemesterYear = payload.year;
+  const currentStudentSemesterCod = payload.code;
+
+  // last Student Year = 2023 === current Student  Year === 2023 and  last Student Code === 01 === current Student Code === 01 , then incrementId 
+
+  if(lastStudentId && (lastStudentSemesterYear === currentStudentSemesterYear) && (lastStudentSemesterCode === currentStudentSemesterCod)){
+    currentId =  lastStudentId.substring(6)
+  }
+
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
