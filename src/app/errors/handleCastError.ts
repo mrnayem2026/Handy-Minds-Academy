ba90e2b640792c3c4d2bf6acde364bbfec2f4e0c
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-12-11 10:42:29 +0600

    handly invalid id

---
+++ src/app/errors/handleCastError.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { TErrorSources, TGenericErrorResponse } from "../interface/error";
+
+export const handleCastError=(err:mongoose.Error.CastError):TGenericErrorResponse=>{
+
+    const match = err.message.match(/"([^"]*)"/);
+    const id = match && match[1];
+  
+    const errorSources: TErrorSources = [
+      {
+        path: err.path,
+        message: `This ${id} id is invalid.`
+      },
+    ];
+
+    const statusCode = 400;
+    return {
+        statusCode,
+        message: "Invalid Id",
+        errorSources
+    }
+}
--- src/app/midellwares/globalErrorHandler.ts
+++ src/app/midellwares/globalErrorHandler.ts
@@ -9,6 +9,7 @@ import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import { handleDuplicateError } from '../errors/handleDuplicateError';
 import { handleValidationError } from '../errors/handleValidationError';
+import { handleCastError } from '../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
@@ -35,6 +36,11 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorSources = simplifiedError.errorSources;
+  } else if (err?.name === 'CastError'){
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorSources = simplifiedError.errorSources
   }
 
   return res.status(statusCode).json({
