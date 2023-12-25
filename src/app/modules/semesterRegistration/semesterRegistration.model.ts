6321631c95e93338402914700b6e004a4bf22175
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-12-25 20:39:51 +0600

    Create interface,model and controller for semester registration

---
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from 'mongoose';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
+
+const semesterRegistrationSchema = new Schema<TSemesterRegistration>(
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: 'AcademicSemester',
+    },
+    status: {
+      type: String,
+      enum: SemesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCreadit: {
+      type: Number,
+      default: 3,
+    },
+    maxCreadit: {
+      type: Number,
+      default: 15,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const semesterRegistration = mongoose.model<TSemesterRegistration>(
+  'semesterRegistration',
+  semesterRegistrationSchema,
+);
