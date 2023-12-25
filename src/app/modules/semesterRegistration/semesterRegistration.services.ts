7e3005ece09cd1b281a647f81c37b107f5ff38bc
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-12-25 21:35:30 +0600

    All bussiness logic donefor createSemesterRegistrationIntoDB

--- src/app/modules/semesterRegistration/semesterRegistration.services.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.services.ts
@@ -3,6 +3,7 @@ import { AppError } from '../../errors/AppError';
 import { RegistrationStatus } from './semesterRegistration.constant';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import { semesterRegistration } from './semesterRegistration.model';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
 
 const createSemesterRegistrationIntoDB = async (
   payload: TSemesterRegistration,
@@ -14,6 +15,9 @@ const createSemesterRegistrationIntoDB = async (
    * Step4: Create the semester registration
    */
 
+
+  const academicSemester = payload?.academicSemester;
+
   const isThereAnyUpcomingOrOngoingSEmester = await semesterRegistration.findOne({
     $or:[
         {status : RegistrationStatus.UPCOMING },
@@ -24,6 +28,27 @@ const createSemesterRegistrationIntoDB = async (
   if(isThereAnyUpcomingOrOngoingSEmester){
     throw new AppError(httpStatus.BAD_REQUEST,`There is allready an ${isThereAnyUpcomingOrOngoingSEmester.status} registered semester !`)
   }
+
+   // check if the semester is exist
+   const isAcademicSemesterExists = await AcademicSemester.findById(academicSemester);
+
+   if (!isAcademicSemesterExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This academic semester not found !',
+    );
+  }
+
+    // check if the semester is already registered!
+    const isSemesterRegistrationExists = await semesterRegistration.findOne({academicSemester})
+
+    if (isSemesterRegistrationExists) {
+        throw new AppError(
+          httpStatus.CONFLICT,
+          'This semester is already registered!',
+        );
+      }
+
 
   const result = await semesterRegistration.create(payload);
 
